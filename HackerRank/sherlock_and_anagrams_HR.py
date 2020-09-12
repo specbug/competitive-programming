9cd9e4bffa82d8158cbb451e02dcb757ead786cc
Rishit Vora <rishitvora@RZT089s-MacBook-Pro.local>
2020-09-12 23:34:49 +0530

    gitignore

--- HackerRank/sherlock_and_anagrams_HR.py
+++ HackerRank/sherlock_and_anagrams_HR.py
@@ -1,17 +1,19 @@
 import itertools
 
 def sherlockAndAnagrams(s):
-	ss = ''.join([str(i) for i in range(len(s))])
-	all_comb = [[''.join(list(p)) for p in itertools.combinations(ss, i)] for i in range(2, len(ss)+1)]
-	n_anagrams = 0
-	for c in all_comb:
-		print(c)
-		# for k in c:
-		# 	print(list(itertools.permutations(k, 2)))
-		# 	print()
-		# print()
+	fc = {}
+	rev_fc = {}
 
-	return n_anagrams
+	rev_s = s[::-1]
+	for i in range(len(s)):
+		for j in range(len(s[i:])):
+			fc[s[i: i+j+1]] = fc.get(s[i: i+j+1], 0) + 1
+			rev_fc[rev_s[i: i+j+1]] = rev_fc.get(rev_s[i: i+j+1], 0) + 1
+
+	print(fc)
+	print()
+	print(rev_fc)
+	return None
 
 t = int(input())
 
