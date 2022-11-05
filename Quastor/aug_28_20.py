bcbaf0c5fee309ce0f1f4d0181d3136870a22017
Rishit Vora <rishit7.rv@gmail.com>
2022-11-05 20:51:24 +0000

    aug_28_20

--- Quastor/aug_28_20.py
+++ Quastor/aug_28_20.py
@@ -2,8 +2,36 @@
 https://quastor.substack.com/p/rusty-days-ahead
 """
 
-def get_permutations_substring(s, p):
-  p_map = dict()
-  s_map = dict()
-  for c in p:
-    i
+def findAnagrams(s: str, p: str):
+    p_map = dict()
+    for c in p:
+        p_map[c] = p_map.get(c, 0) + 1
+    s_map = dict()
+    ctr = 0
+    l = len(p)
+    n = len(s)
+    if n < l:
+        return []
+    ans = []
+    for i in range(l):
+        s_map[s[i]] = s_map.get(s[i], 0) + 1
+        if s[i] in p_map and p_map[s[i]] >= s_map[s[i]]:
+            ctr += 1
+        if ctr == l:
+            ans.append(0)
+    for i in range(l, n):
+        we = s[i]
+        ws = s[i-l]
+        if ws in p_map and p_map[ws] >= s_map.get(ws, -1):
+            ctr -= 1
+        if ws in s_map:
+            s_map[ws] -= 1
+        s_map[we] = s_map.get(we, 0) + 1
+        if we in p_map and p_map[we] >= s_map[we]:
+            ctr += 1
+        if ctr == l:
+            ans.append(i-l+1)
+    return ans
+
+print(findAnagrams('cbaebabacd', 'abc') == [0,6])
+print(findAnagrams('abab', 'ab') == [0,1,2])
