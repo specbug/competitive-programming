1141ec5598db865d150a84a508a1984672416fa8
Rishit Vora <rishit7.rv@gmail.com>
2022-12-06 06:39:36 +0000

    aoc 2022: day 6

---
+++ AdventOfCode/2022/6_2.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+def main(file):
+	ans = -1
+	wstart = 0
+	wend = 0
+	with open(file) as f:
+		for l in f.readlines():
+			l = l.strip().strip('\n')
+			for e, c in enumerate(l):
+				wend = e
+				if (wend-wstart+1) < 14:
+					continue
+				if len(set(l[wstart:wend+1])) == 14:
+					return wend+1
+				else:
+					wstart += 1
+	return ans
+
+
+file = sys.argv[1]
+print(main(file))
