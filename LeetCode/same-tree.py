052e209813575a1af2c3054afe7e72f5bfd5b857
Rishit Vora <rishit7.rv@gmail.com>
2023-01-10 06:34:24 +0000

    same-tree

--- LeetCode/same-tree.py
+++ LeetCode/same-tree.py
@@ -5,19 +5,11 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def preorder_traversal(self, node: TreeNode):
-        if node is None:
-            return [None]
-        ans = [node.val]
-        ans += self.preorder_traversal(node.left)
-        ans += self.preorder_traversal(node.right)
-        return ans
-
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if p is None and q is None:
             return True
         if p is None or q is None:
             return False
-        p_tree = self.preorder_traversal(p)
-        q_tree = self.preorder_traversal(q)
-        return p_tree == q_tree
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
