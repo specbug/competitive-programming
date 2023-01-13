222801ae577098c72303d171f84f7369d119ccf0
Rishit Vora <rishit7.rv@gmail.com>
2023-01-13 08:53:00 +0000

    sliding-window-maximum optimised

--- LeetCode/sliding-window-maximum.py
+++ LeetCode/sliding-window-maximum.py
@@ -1,51 +1,4 @@
-class Queue:
-	def __init__(self):
-		self._queue = []
-		self._ptop = 0
-		self._len = 0
-
-	def __repr__(self):
-		_repr = self._queue[self._ptop:].__repr__().strip('[').strip(']')
-		return ' -> '.join(_repr.split(', '))
-
-	def enqueue(self, val):
-		self._len += 1
-		self._queue.append(val)
-
-	def __clear(self):
-		self._queue = []
-		self._ptop = 0
-		self._len = 0
-
-	def dequeue(self):
-		val = self._queue[self._ptop]
-		self._ptop += 1
-		if self._ptop == self._len:
-			self.__clear()
-		return val
-
-	def pop(self):
-		val = self._queue.pop()
-		self._len -= 1
-		if self._ptop == self._len:
-			self.__clear()
-		return val
-
-	def peek(self):
-		if len(self) == 0:
-			return None
-		return self._queue[self._ptop]
-
-	def peek_last(self):
-		if len(self) == 0:
-			return None
-		return self._queue[self._len-1]
-
-	def is_empty(self):
-		return len(self) == 0
-
-	def __len__(self):
-		return self._len - self._ptop
+from collections import deque
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
@@ -55,19 +8,19 @@ class Solution:
         if n == 1:
             return nums[0]
         k = min(n, k)
-        queue = Queue()
+        queue = deque()
         ans = []
         for i in range(k):
-            while not queue.is_empty() and nums[i] > nums[queue.peek_last()]:
+            while queue and nums[i] > nums[queue[-1]]:
                 queue.pop()
-            queue.enqueue(i)
-        ans.append(nums[queue.peek()])
+            queue.append(i)
+        ans.append(nums[queue[0]])
 
         for i in range(n-k):
-            if queue.peek() == i:
-                queue.dequeue()
-            while not queue.is_empty() and nums[i+k] > nums[queue.peek_last()]:
+            if queue[0] == i:
+                queue.popleft()
+            while queue and nums[i+k] > nums[queue[-1]]:
                 queue.pop()
-            queue.enqueue(i+k)
-            ans.append(nums[queue.peek()])
+            queue.append(i+k)
+            ans.append(nums[queue[0]])
         return ans
