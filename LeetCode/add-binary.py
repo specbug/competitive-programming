d6641a03b5bfb4e602064cfba59c8aec4b9ea94f
Rishit Vora <rishit7.rv@gmail.com>
2023-01-18 17:17:58 +0000

    add-binary opt

--- LeetCode/add-binary.py
+++ LeetCode/add-binary.py
@@ -1,24 +1,22 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-            m = len(a)
-            n = len(b)
-            l = max(m, n)
-            a = '0' * (l - m) + a[:]
-            b = '0' * (l - n) + b[:]
-            ans = []
+        ans = ''
+        m = len(a)
+        n = len(b)
+        l = max(m, n)
+        a = '0'*(l-m) + a
+        b = '0'*(l-n) + b
+        c = 0
+        imap = {'1': 1, '0': 0}
+        for i in range(l-1, -1, -1):
+            val = imap[a[i]] + imap[b[i]] + c
             c = 0
-            for i in range(l-1, -1, -1):
-                _a = int(a[i])
-                _b = int(b[i])
-                _ans = _a + _b + c
-                if _ans > 1:
-                    c = 1
-                else:
-                    c = 0
-                if _ans & 1 == 0:
-                    ans.append('0')
-                else:
-                    ans.append('1')
-            if c > 0:
-                ans.append('1')
-            return ''.join(ans[::-1])
+            c = int(val>=2)
+            if val >= 3:
+                ans += '1'
+            else:
+                ans += str(val&1)
+        while c != 0:
+            ans += '1'
+            c -= 1
+        return ans[::-1]
