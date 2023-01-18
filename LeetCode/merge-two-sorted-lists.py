17c3eded8a9c120fbcf10d9877cecdf88c7edc31
Rishit Vora <rishit7.rv@gmail.com>
2023-01-18 18:23:49 +0000

    merge-two-sorted-lists

--- LeetCode/merge-two-sorted-lists.py
+++ LeetCode/merge-two-sorted-lists.py
@@ -9,25 +9,15 @@ class Solution:
             return list2
         if not list2:
             return list1
-        h1 = list1
-        h2 = list2
-        if h1.val <= h2.val:
-            temp = h1
-            h1 = h1.next
-        else:
-            temp = h2
-            h2 = h2.next
-        ans = temp
-        while h1 and h2:
-            if h1.val <= h2.val:
-                temp.next = h1
-                h1 = h1.next
+        ans = temp = ListNode()
+        while list1 and list2:
+            if list1.val <= list2.val:
+                temp.next = list1
+                list1 = list1.next
             else:
-                temp.next = h2
-                h2 = h2.next
+                temp.next = list2
+                list2 = list2.next
             temp = temp.next
-        if not h1:
-            temp.next = h2
-        if not h2:
-            temp.next = h1
-        return ans
+        if list1 or list2:
+            temp.next = list1 if list1 else list2
+        return ans.next
